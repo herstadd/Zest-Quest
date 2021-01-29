b76eb24fb47f5bf8a7340dcbda31f3603d039837
Sam Borhan <samborhan1566@gmail.com>
2021-01-29 11:39:55 -0800

    Added "CharacterDeletePage.xaml" and "CharacterDeletePage.xaml.cs" inside Views/Characters folder at Game.csproj

---
+++ Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Read Page
+    /// </summary>
+    [DesignTimeVisible(false)] 
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterDeletePage : ContentPage
+    {
+        // View Model for Item
+        readonly GenericViewModel<ItemModel> viewModel;
+
+        // Empty Constructor for UTs
+        public CharacterDeletePage(bool UnitTest) { }
+
+        // Constructor for Delete takes a view model of what to delete
+        public CharacterDeletePage(GenericViewModel<ItemModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = data;
+
+            this.viewModel.Title = "Delete " + data.Title;
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Delete_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Delete", viewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Trap the Back Button on the Phone
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            // Add your code here...
+            return true;
+        }
+    }
+}
