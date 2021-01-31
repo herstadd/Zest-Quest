6328539900f08a1b3909c72f0aec7274e686c3e2
JungBok Cho <chojb2015@gmail.com>
2021-01-30 23:07:51 -0800

    Add new lists of abilities for new chef types in AbilityEnum.cs

--- Game/Game/Models/Enum/AbilityEnum.cs
+++ Game/Game/Models/Enum/AbilityEnum.cs
@@ -126,7 +126,7 @@ namespace Game.Models
         /// <summary>
         /// Returns a list of strings of the enum for Fighter
         /// </summary>
-        public static List<string> GetListFighter
+        public static List<string> GetListHeadChef
         {
             get
             {
@@ -144,7 +144,83 @@ namespace Game.Models
         /// <summary>
         /// Returns a list of strings of the enum for Cleric
         /// </summary>
-        public static List<string> GetListCleric
+        public static List<string> GetListSousChef
+        {
+            get
+            {
+
+                List<string> AbilityList = new List<string>{
+                AbilityEnum.Quick.ToString(),
+                AbilityEnum.Barrier.ToString(),
+                AbilityEnum.Curse.ToString(),
+                AbilityEnum.Heal.ToString()
+                };
+
+                AbilityList.AddRange(GetListOthers);
+                return AbilityList;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of strings of the enum for Fighter
+        /// </summary>
+        public static List<string> GetListSchoolChef
+        {
+            get
+            {
+                List<string> AbilityList = new List<string>{
+                AbilityEnum.Nimble.ToString(),
+                AbilityEnum.Toughness.ToString(),
+                AbilityEnum.Focus.ToString()
+                };
+
+                AbilityList.AddRange(GetListOthers);
+                return AbilityList;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of strings of the enum for Cleric
+        /// </summary>
+        public static List<string> GetListSushiChef
+        {
+            get
+            {
+
+                List<string> AbilityList = new List<string>{
+                AbilityEnum.Quick.ToString(),
+                AbilityEnum.Barrier.ToString(),
+                AbilityEnum.Curse.ToString(),
+                AbilityEnum.Heal.ToString()
+                };
+
+                AbilityList.AddRange(GetListOthers);
+                return AbilityList;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of strings of the enum for Fighter
+        /// </summary>
+        public static List<string> GetListCatChef
+        {
+            get
+            {
+                List<string> AbilityList = new List<string>{
+                AbilityEnum.Nimble.ToString(),
+                AbilityEnum.Toughness.ToString(),
+                AbilityEnum.Focus.ToString()
+                };
+
+                AbilityList.AddRange(GetListOthers);
+                return AbilityList;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of strings of the enum for Cleric
+        /// </summary>
+        public static List<string> GetListHomeCook
         {
             get
             {
