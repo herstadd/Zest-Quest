6ca524498c8966c4dfb2156def9b19e01571fb79
JungBok Cho <chojb2015@gmail.com>
2021-02-02 22:41:19 -0800

    Add MonsterUpdatePage.xaml and MonsterUpdatePage.xaml.cs to Project

---
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Item Update Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterUpdatePage : ContentPage
+    {
+        // View Model for Item
+        public readonly GenericViewModel<CharacterModel> ViewModel;
+
+        // Empty Constructor for Tests
+        public MonsterUpdatePage(bool UnitTest){ }
+
+        /// <summary>
+        /// Constructor that takes and existing data item
+        /// </summary>
+        public MonsterUpdatePage(GenericViewModel<CharacterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Update " + data.Title;
+
+            //Need to make the SelectedItem a string, so it can select the correct item.
+            JobPicker.SelectedItem = ViewModel.Data.Job.ToString();
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Update_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
+            }
+
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Update Level value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void LevelValueChanged(object sender, EventArgs e)
+        {
+            int NewLevelValue = Int32.Parse(LevelValue.Text);
+            LevelValue.Text = ValueChange((sender as Button).Text, NewLevelValue, true).ToString();
+        }
+
+        /// <summary>
+        /// Update Attack value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void AttackValueChanged(object sender, EventArgs e)
+        {
+            int NewAttackValue = Int32.Parse(AttackValue.Text);
+            AttackValue.Text = ValueChange((sender as Button).Text, NewAttackValue, false).ToString();
+        }
+
+        /// <summary>
+        /// Update Defense value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DefenseValueChanged(object sender, EventArgs e)
+        {
+            int NewDefenseValue = Int32.Parse(DefenseValue.Text);
+            DefenseValue.Text = ValueChange((sender as Button).Text, NewDefenseValue, false).ToString();
+        }
+
+        /// <summary>
+        /// Update Speed value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void SpeedValueChanged(object sender, EventArgs e)
+        {
+            int NewSpeedValue = Int32.Parse(SpeedValue.Text);
+            SpeedValue.Text = ValueChange((sender as Button).Text, NewSpeedValue, false).ToString();
+        }
+
+        /// <summary>
+        /// Update Max Health value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void MaxHealthValueChanged(object sender, EventArgs e)
+        {
+            int NewMaxHealthValue = Int32.Parse(MaxHealthValue.Text);
+            MaxHealthValue.Text = ValueChange((sender as Button).Text, NewMaxHealthValue, false).ToString();
+        }
+
+        /// <summary>
+        /// Change Stat value
+        /// </summary>
+        /// <param name="ButtonText"></param>
+        /// <param name="num"></param>
+        /// <param name="IsLevel"></param>
+        /// <returns></returns>
+        private int ValueChange(String ButtonText, int num, bool IsLevel)
+        {
+            if (ButtonText.Equals("-"))
+            {
+                num--;
+                if (IsLevel)
+                {
+                    return num < 1 ? 1 : num;
+                }
+                else
+                {
+                    return num < 0 ? 0 : num;
+                }
+            }
+            else
+            {
+                return num + 1;
+            }
+        }
+    }
+}
