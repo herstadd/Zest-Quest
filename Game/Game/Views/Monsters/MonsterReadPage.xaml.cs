365261da6cd25c2fc643fb2c5fa8f87a3e6a1720
JungBok Cho <chojb2015@gmail.com>
2021-02-02 22:34:35 -0800

    Added MonsterReadPage.xaml and MonsterReadPage.xaml.cs to Project

---
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Read Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterReadPage : ContentPage
+    {
+        // View Model for Item
+        public readonly GenericViewModel<CharacterModel> ViewModel;
+
+        // Empty Constructor for UTs
+        public MonsterReadPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public MonsterReadPage(GenericViewModel<CharacterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Update_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterUpdatePage(ViewModel)));
+            await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Calls for Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Delete_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(ViewModel)));
+            await Navigation.PopAsync();
+        }
+    }
+}
