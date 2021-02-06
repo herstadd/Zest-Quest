b65802a178d95b7971f0482aefd52a1fe577d4d1
Sam Borhan <samborhan1566@gmail.com>
2021-02-05 17:49:50 -0800

    Modified Monster Update Page to match with the wirefram, It changed "MosterUpdatePage.xaml" and "MosterUpdatePage.xaml.cs"

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -18,6 +18,22 @@ namespace Game.Views
         // View Model for Item
         public readonly GenericViewModel<MonsterModel> ViewModel;
 
+        // Current Monster Name
+        public String CurrentName { set; get; }
+
+        // Current Monster Type
+        public MonsterTypeEnum CurrentType { set; get; }
+
+        // Current Monster Description
+        public String CurrentDescription { set; get; }
+
+        // Current Mosnter Unique Drop
+        public SpecialDropEnum CurrentUiqueDrop { set; get; }
+
+        // Current Monster Class (Boss or Standard)
+        public String CurrentClass { set; get; }
+
+
         // Empty Constructor for Tests
         public MonsterUpdatePage(bool UnitTest){ }
 
@@ -33,8 +49,29 @@ namespace Game.Views
             this.ViewModel.Title = "Update " + data.Title;
 
             //Need to make the SelectedItem a string, so it can select the correct item.
-            JobPicker.SelectedItem = ViewModel.Data.Job.ToString();
+            JobPicker.SelectedItem = ViewModel.Data.MonsterType.ToString();
+
+            // Storing all current values to use them if user decide to cancel
+            CurrentName = ViewModel.Data.Name;
+            CurrentType = ViewModel.Data.MonsterType;
+            CurrentDescription = ViewModel.Data.Description;
+            CurrentUiqueDrop = ViewModel.Data.SpecialDrop; 
         }
+
+
+        /// <summary>
+        /// Changes the item for a Monster based on the type that is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void TypeChanged(object sender, EventArgs e)
+        {
+            var selected = MonsterJobEnumHelper.ConvertStringToEnum((String)JobPicker.SelectedItem);
+            UniqueDrop.Text = MonsterJobEnumExtensions.GetSpecialDrop(selected);
+            ViewModel.Data.SpecialDrop = SpecialDropEnumHelper.ConvertStringToEnum(UniqueDrop.Text);
+            MonsterClass.Text = MonsterJobEnumExtensions.GetMonsterClass(selected);
+        }
+
 
         /// <summary>
         /// Save calls to Update
@@ -66,89 +103,13 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Cancel_Clicked(object sender, EventArgs e)
         {
+            // Restore all values to what they were 
+            ViewModel.Data.Name = CurrentName;
+            ViewModel.Data.MonsterType = CurrentType;
+            ViewModel.Data.Description = CurrentDescription;
+            ViewModel.Data.SpecialDrop = CurrentUiqueDrop;
+
             await Navigation.PopModalAsync();
-        }
-
-        /// <summary>
-        /// Update Level value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void LevelValueChanged(object sender, EventArgs e)
-        {
-            int NewLevelValue = Int32.Parse(LevelValue.Text);
-            LevelValue.Text = ValueChange((sender as Button).Text, NewLevelValue, true).ToString();
-        }
-
-        /// <summary>
-        /// Update Attack value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void AttackValueChanged(object sender, EventArgs e)
-        {
-            int NewAttackValue = Int32.Parse(AttackValue.Text);
-            AttackValue.Text = ValueChange((sender as Button).Text, NewAttackValue, false).ToString();
-        }
-
-        /// <summary>
-        /// Update Defense value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void DefenseValueChanged(object sender, EventArgs e)
-        {
-            int NewDefenseValue = Int32.Parse(DefenseValue.Text);
-            DefenseValue.Text = ValueChange((sender as Button).Text, NewDefenseValue, false).ToString();
-        }
-
-        /// <summary>
-        /// Update Speed value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void SpeedValueChanged(object sender, EventArgs e)
-        {
-            int NewSpeedValue = Int32.Parse(SpeedValue.Text);
-            SpeedValue.Text = ValueChange((sender as Button).Text, NewSpeedValue, false).ToString();
-        }
-
-        /// <summary>
-        /// Update Max Health value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void MaxHealthValueChanged(object sender, EventArgs e)
-        {
-            int NewMaxHealthValue = Int32.Parse(MaxHealthValue.Text);
-            MaxHealthValue.Text = ValueChange((sender as Button).Text, NewMaxHealthValue, false).ToString();
-        }
-
-        /// <summary>
-        /// Change Stat value
-        /// </summary>
-        /// <param name="ButtonText"></param>
-        /// <param name="num"></param>
-        /// <param name="IsLevel"></param>
-        /// <returns></returns>
-        private int ValueChange(String ButtonText, int num, bool IsLevel)
-        {
-            if (ButtonText.Equals("-"))
-            {
-                num--;
-                if (IsLevel)
-                {
-                    return num < 1 ? 1 : num;
-                }
-                else
-                {
-                    return num < 0 ? 0 : num;
-                }
-            }
-            else
-            {
-                return num + 1;
-            }
         }
     }
 }
