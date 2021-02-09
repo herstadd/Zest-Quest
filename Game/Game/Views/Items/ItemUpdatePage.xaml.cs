f1523b50d2d05fe302842b14f2bb7d07f09abfa9
Sam Borhan <samborhan1566@gmail.com>
2021-02-09 01:45:21 -0800

    Disabled Update button and pop up an alert message when Name Entry Box is empty in ItemUpdatePage.xaml.cs

--- Game/Game/Views/Items/ItemUpdatePage.xaml.cs
+++ Game/Game/Views/Items/ItemUpdatePage.xaml.cs
@@ -76,6 +76,22 @@ namespace Game.Views
                 ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
             }
 
+            // If the Name in the data box is empty then data won't update 
+            if (string.IsNullOrEmpty(ViewModel.Data.Name))
+            {
+                // pop message appears when name box is empty
+                await DisplayAlert("Alert", "Name is Empty!", "OK");
+                return;
+            }
+
+            // If the Name in the data box is just white space then data won't update 
+            if (string.IsNullOrWhiteSpace(ViewModel.Data.Name))
+            {
+                // pop message appears when name box is just white spaces
+                await DisplayAlert("Alert", "Name is Empty!", "OK");
+                return;
+            }
+
             MessagingCenter.Send(this, "Update", ViewModel.Data);
             await Navigation.PopModalAsync();
         }
