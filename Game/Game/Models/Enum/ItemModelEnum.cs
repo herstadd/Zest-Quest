7fe5841f4a874bd96e59d13018c5ef20feed682f
JungBok Cho <chojb2015@gmail.com>
2021-02-11 17:37:12 -0800

    Add Helper functions in ItemModelEnum.cs

--- Game/Game/Models/Enum/ItemModelEnum.cs
+++ Game/Game/Models/Enum/ItemModelEnum.cs
@@ -163,8 +163,27 @@ namespace Game.Models
     /// </summary>
     public static class ItemModelEnumHelper
     {
-       
+        /// <summary>
+        /// Returns a list of strings of the enum for Attribute
+        /// Removes the attributes that are not changable by Items such as Unknown, MaxHealth
+        /// </summary>
+        public static List<string> GetListItem
+        {
+            get
+            {
+                var myList = Enum.GetNames(typeof(ItemModelEnum)).ToList().Where(m => m.ToString().Equals("Unknown") == false).ToList();
+                return myList;
+            }
+        }
 
-       
+        /// <summary>
+        /// Given the String for an enum, return its value.  That allows for the enums to be numbered 2,4,6 rather than 1,2,3
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ItemModelEnum ConvertStringToEnum(string value)
+        {
+            return (ItemModelEnum)Enum.Parse(typeof(ItemModelEnum), value);
+        }
     }
 }
