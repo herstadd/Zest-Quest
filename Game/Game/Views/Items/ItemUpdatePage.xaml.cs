55d332ee52c3197040014da2505be62749d0075d
JungBok Cho <chojb2015@gmail.com>
2021-02-13 15:46:38 -0800

    Update Type_changed function in ItemUpdatePage.xaml.cs

--- Game/Game/Views/Items/ItemUpdatePage.xaml.cs
+++ Game/Game/Views/Items/ItemUpdatePage.xaml.cs
@@ -52,8 +52,21 @@ namespace Game.Views
         public void Type_Changed(object sender, EventArgs e)
         {
             // Change bellow according to our upcoming Item name Enum
-            var selected = ItemModelEnumHelper.ConvertStringToEnum((String)ItemTypeEntry.SelectedItem);
-            ViewModel.Data.Type = selected;
+            var selected = (String)ItemTypeEntry.SelectedItem;
+
+            Description.Text = ItemIndexViewModel.Instance.GetDescription(selected);
+            this.ViewModel.Data.Description = ItemIndexViewModel.Instance.GetDescription(selected);
+            PictureSource.Source = ItemIndexViewModel.Instance.GetImage(selected);
+            this.ViewModel.Data.Type = ItemIndexViewModel.Instance.GetType(selected);
+
+            // Location
+            //LocationPicker.SelectedItem = ItemIndexViewModel.Instance.GetLocation(selected);
+            // Attribute
+            //AttributePicker.SelectedItem = ItemIndexViewModel.Instance.GetAttribute(selected);
+
+            RangeValue.Text = ItemIndexViewModel.Instance.GetValues(selected, "Range").ToString();
+            DamageValue.Text = ItemIndexViewModel.Instance.GetValues(selected, "Damage").ToString();
+            ValueValue.Text = ItemIndexViewModel.Instance.GetValues(selected, "Value").ToString();
         }
 
         /// <summary>
