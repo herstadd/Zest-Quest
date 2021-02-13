8bae6f5a53617b0960ac14dde20dd58c60e82258
JungBok Cho <chojb2015@gmail.com>
2021-02-13 14:10:32 -0800

    Uncomment for testing in DescriptionEnum.cs

--- Game/Game/Models/Enum/DescriptionEnum.cs
+++ Game/Game/Models/Enum/DescriptionEnum.cs
@@ -1,238 +1,238 @@
-﻿using Game.Helpers;
+﻿//using Game.Helpers;
 
-namespace Game.Models
-{
-    /// <summary>
-    /// The Types of s a Action can have
-    /// Used in Action Crudi, and in Battles.
-    /// </summary>
-    public enum DescriptionEnum
-    {
-        // Not specified
-        Unknown = 0,
+//namespace Game.Models
+//{
+//    /// <summary>
+//    /// The Types of s a Action can have
+//    /// Used in Action Crudi, and in Battles.
+//    /// </summary>
+//    public enum DescriptionEnum
+//    {
+//        // Not specified
+//        Unknown = 0,
 
-        // HeadChef
-        HeadChef = 1,
+//        // HeadChef
+//        HeadChef = 1,
 
-        // SousChef
-        SousChef = 10,
+//        // SousChef
+//        SousChef = 10,
 
-        // SchoolChef
-        SchoolChef = 20,
+//        // SchoolChef
+//        SchoolChef = 20,
 
-        // SushiChef
-        SushiChef = 30,
+//        // SushiChef
+//        SushiChef = 30,
 
-        // CatChef
-        CatChef = 40,
+//        // CatChef
+//        CatChef = 40,
 
-        // HomeCook
-        HomeCook = 50
-    }
+//        // HomeCook
+//        HomeCook = 50
+//    }
 
-    /// <summary>
-    /// Friendly strings for the Enum Class
-    /// </summary>
-    public static class DescriptionEnumExtensions
-    {
-        /// <summary>
-        /// Display a String for the Enums
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static string ToMessage(this DescriptionEnum value)
-        {
-            // Default String
-            var Message = "None";
+//    /// <summary>
+//    /// Friendly strings for the Enum Class
+//    /// </summary>
+//    public static class DescriptionEnumExtensions
+//    {
+//        /// <summary>
+//        /// Display a String for the Enums
+//        /// </summary>
+//        /// <param name="value"></param>
+//        /// <returns></returns>
+//        public static string ToMessage(this DescriptionEnum value)
+//        {
+//            // Default String
+//            var Message = "None";
 
-            switch (value)
-            {
-                case DescriptionEnum.HeadChef:
-                    Message = "Each item grants this character double the normal stat modifier";
-                    break;
+//            switch (value)
+//            {
+//                case DescriptionEnum.HeadChef:
+//                    Message = "Each item grants this character double the normal stat modifier";
+//                    break;
 
-                case DescriptionEnum.SousChef:
-                    Message = "This character's attack attribute will be 3 times stronger than usual for the first attack in every round";
-                    break;
+//                case DescriptionEnum.SousChef:
+//                    Message = "This character's attack attribute will be 3 times stronger than usual for the first attack in every round";
+//                    break;
 
-                case DescriptionEnum.SchoolChef:
-                    Message = "Provide a 20% attack buff to the rest of team if the school chef dies in a battle";
-                    break;
+//                case DescriptionEnum.SchoolChef:
+//                    Message = "Provide a 20% attack buff to the rest of team if the school chef dies in a battle";
+//                    break;
 
-                case DescriptionEnum.SushiChef:
-                    Message = "Has the ability to attack from anywhere on the map with any item";
-                    break;
+//                case DescriptionEnum.SushiChef:
+//                    Message = "Has the ability to attack from anywhere on the map with any item";
+//                    break;
 
-                case DescriptionEnum.CatChef:
-                    Message = "Has nine lives (so if character dies, comes back to life 8 more times,) but cannot hold more than one item at a time";
-                    break;
+//                case DescriptionEnum.CatChef:
+//                    Message = "Has nine lives (so if character dies, comes back to life 8 more times,) but cannot hold more than one item at a time";
+//                    break;
 
-                case DescriptionEnum.HomeCook:
-                    Message = "After winning a battle their current health will be recovered by 10% of original max health up to max health";
-                    break;
+//                case DescriptionEnum.HomeCook:
+//                    Message = "After winning a battle their current health will be recovered by 10% of original max health up to max health";
+//                    break;
 
-                case DescriptionEnum.Unknown:
-                default:
-                    break;
-            }
+//                case DescriptionEnum.Unknown:
+//                default:
+//                    break;
+//            }
 
-            return Message;
-        }
+//            return Message;
+//        }
 
-        /// <summary>
-        /// Gets the name of the picture that is associated with the Chef type
-        /// </summary>
-        /// <param name="value">The type of chef</param>
-        /// <returns>The name of the associated image</returns>
-        public static string GetPicture(this DescriptionEnum value)
-        {
-            // Default String
-            var Picture = "None";
+//        /// <summary>
+//        /// Gets the name of the picture that is associated with the Chef type
+//        /// </summary>
+//        /// <param name="value">The type of chef</param>
+//        /// <returns>The name of the associated image</returns>
+//        public static string GetPicture(this DescriptionEnum value)
+//        {
+//            // Default String
+//            var Picture = "None";
 
-            switch (value)
-            {
-                case DescriptionEnum.HeadChef:
-                    Picture = "headchef.png";
-                    break;
+//            switch (value)
+//            {
+//                case DescriptionEnum.HeadChef:
+//                    Picture = "headchef.png";
+//                    break;
 
-                case DescriptionEnum.SousChef:
-                    Picture = "souschef.png";
-                    break;
+//                case DescriptionEnum.SousChef:
+//                    Picture = "souschef.png";
+//                    break;
 
-                case DescriptionEnum.SchoolChef:
-                    Picture = "schoolchef.png";
-                    break;
+//                case DescriptionEnum.SchoolChef:
+//                    Picture = "schoolchef.png";
+//                    break;
 
-                case DescriptionEnum.SushiChef:
-                    Picture = "sushichef.png";
-                    break;
+//                case DescriptionEnum.SushiChef:
+//                    Picture = "sushichef.png";
+//                    break;
 
-                case DescriptionEnum.CatChef:
-                    Picture = "catchef.png";
-                    break;
+//                case DescriptionEnum.CatChef:
+//                    Picture = "catchef.png";
+//                    break;
 
-                case DescriptionEnum.HomeCook:
-                    Picture = "homechef.png";
-                    break;
+//                case DescriptionEnum.HomeCook:
+//                    Picture = "homechef.png";
+//                    break;
 
-                case DescriptionEnum.Unknown:
-                default:
-                    break;
-            }
-            return Picture;
-        }
+//                case DescriptionEnum.Unknown:
+//                default:
+//                    break;
+//            }
+//            return Picture;
+//        }
 
-        /// <summary>
-        /// Gets the max health of a character given their DescriptionEnum value
-        /// </summary>
-        /// <param name="value">The DescriptionEnum value of the chef</param>
-        /// <returns>The maxHealth of the chef</returns>
-        public static string GetDefaultLevel(this DescriptionEnum value)
-        {
-            int DefaultLevel = 0;
+//        /// <summary>
+//        /// Gets the max health of a character given their DescriptionEnum value
+//        /// </summary>
+//        /// <param name="value">The DescriptionEnum value of the chef</param>
+//        /// <returns>The maxHealth of the chef</returns>
+//        public static string GetDefaultLevel(this DescriptionEnum value)
+//        {
+//            int DefaultLevel = 0;
 
-            switch (value)
-            {
-                case DescriptionEnum.HeadChef:
-                case DescriptionEnum.HomeCook:
-                    DefaultLevel = 10;
-                    break;
+//            switch (value)
+//            {
+//                case DescriptionEnum.HeadChef:
+//                case DescriptionEnum.HomeCook:
+//                    DefaultLevel = 10;
+//                    break;
 
-                case DescriptionEnum.SchoolChef:
-                    DefaultLevel = 5;
-                    break;
+//                case DescriptionEnum.SchoolChef:
+//                    DefaultLevel = 5;
+//                    break;
 
-                case DescriptionEnum.SousChef:
-                case DescriptionEnum.SushiChef:
-                case DescriptionEnum.CatChef:
-                    DefaultLevel = 1;
-                    break;
+//                case DescriptionEnum.SousChef:
+//                case DescriptionEnum.SushiChef:
+//                case DescriptionEnum.CatChef:
+//                    DefaultLevel = 1;
+//                    break;
 
-                case DescriptionEnum.Unknown:
-                default:
-                    break;
-            }
+//                case DescriptionEnum.Unknown:
+//                default:
+//                    break;
+//            }
 
-            return DefaultLevel.ToString();
-        }
+//            return DefaultLevel.ToString();
+//        }
 
 
 
-        /// <summary>
-        /// Converts a string to the enum representation
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static DescriptionEnum ToEnum(string value)
-        {
-            var Type = DescriptionEnum.Unknown;
+//        /// <summary>
+//        /// Converts a string to the enum representation
+//        /// </summary>
+//        /// <param name="value"></param>
+//        /// <returns></returns>
+//        public static DescriptionEnum ToEnum(string value)
+//        {
+//            var Type = DescriptionEnum.Unknown;
 
-            switch (value)
-            {
-                case "HeadChef":
-                    Type = DescriptionEnum.HeadChef;
-                    break;
+//            switch (value)
+//            {
+//                case "HeadChef":
+//                    Type = DescriptionEnum.HeadChef;
+//                    break;
 
-                case "SousChef":
-                    Type = DescriptionEnum.SousChef;
-                    break;
+//                case "SousChef":
+//                    Type = DescriptionEnum.SousChef;
+//                    break;
 
-                case "SchoolChef":
-                    Type = DescriptionEnum.SchoolChef;
-                    break;
+//                case "SchoolChef":
+//                    Type = DescriptionEnum.SchoolChef;
+//                    break;
 
-                case "SushiChef":
-                    Type = DescriptionEnum.SushiChef;
-                    break;
+//                case "SushiChef":
+//                    Type = DescriptionEnum.SushiChef;
+//                    break;
 
-                case "CatChef":
-                    Type = DescriptionEnum.CatChef;
-                    break;
+//                case "CatChef":
+//                    Type = DescriptionEnum.CatChef;
+//                    break;
 
-                case "HomeCook":
-                    Type = DescriptionEnum.HomeCook;
-                    break;
+//                case "HomeCook":
+//                    Type = DescriptionEnum.HomeCook;
+//                    break;
 
-                case "Unknown":
-                default:
-                    break;
-            }
+//                case "Unknown":
+//                default:
+//                    break;
+//            }
 
-            return Type;
-        }
+//            return Type;
+//        }
 
-        /// <summary>
-        /// Calculates a new max health value based on which chef type was selected
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static int CalcMaxHealth(this DescriptionEnum value)
-        {
-            int DefaultLevel = 0;
+//        /// <summary>
+//        /// Calculates a new max health value based on which chef type was selected
+//        /// </summary>
+//        /// <param name="value"></param>
+//        /// <returns></returns>
+//        public static int CalcMaxHealth(this DescriptionEnum value)
+//        {
+//            int DefaultLevel = 0;
 
-            switch (value)
-            {
-                case DescriptionEnum.HeadChef:
-                case DescriptionEnum.HomeCook:
-                    DefaultLevel = DiceHelper.RollDice(10, 10);
-                    break;
+//            switch (value)
+//            {
+//                case DescriptionEnum.HeadChef:
+//                case DescriptionEnum.HomeCook:
+//                    DefaultLevel = DiceHelper.RollDice(10, 10);
+//                    break;
 
-                case DescriptionEnum.SchoolChef:
-                    DefaultLevel = DiceHelper.RollDice(5, 10);
-                    break;
+//                case DescriptionEnum.SchoolChef:
+//                    DefaultLevel = DiceHelper.RollDice(5, 10);
+//                    break;
 
-                case DescriptionEnum.SousChef:
-                case DescriptionEnum.SushiChef:
-                case DescriptionEnum.CatChef:
-                    DefaultLevel = DiceHelper.RollDice(1, 10);
-                    break;
+//                case DescriptionEnum.SousChef:
+//                case DescriptionEnum.SushiChef:
+//                case DescriptionEnum.CatChef:
+//                    DefaultLevel = DiceHelper.RollDice(1, 10);
+//                    break;
 
-                case DescriptionEnum.Unknown:
-                default:
-                    break;
-            }
+//                case DescriptionEnum.Unknown:
+//                default:
+//                    break;
+//            }
 
-            return DefaultLevel;
-        }
-    }
-}
+//            return DefaultLevel;
+//        }
+//    }
+//}
