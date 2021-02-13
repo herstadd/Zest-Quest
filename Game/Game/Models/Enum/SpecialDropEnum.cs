88e4c59b50c23255c3a8783e6ef88ec4c6c76e88
JungBok Cho <chojb2015@gmail.com>
2021-02-13 15:02:06 -0800

    Comment out SpecialDropEnum.cs for testing

--- Game/Game/Models/Enum/SpecialDropEnum.cs
+++ Game/Game/Models/Enum/SpecialDropEnum.cs
@@ -1,124 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Game.Models
-{
-    /// <summary>
-    /// The unique Drop Items can be
-    /// dropped by a Monster in Battles.
-    /// </summary>
-    public enum SpecialDropEnum
-    {
-        // Unknown is not used at the moment, but has no skills
-        Unknown = 0,
+//namespace Game.Models
+//{
+//    /// <summary>
+//    /// The unique Drop Items can be
+//    /// dropped by a Monster in Battles.
+//    /// </summary>
+//    public enum SpecialDropEnum
+//    {
+//        // Unknown is not used at the moment, but has no skills
+//        Unknown = 0,
 
-        // Attribute:Attack | Value:+6 | Location:Hand |Range:5
-        Refrigerator = 10,
+//        // Attribute:Attack | Value:+6 | Location:Hand |Range:5
+//        Refrigerator = 10,
 
-        // Attribute:Health | Value:+4 | Location:Finger |Range:0
-        OnionRing = 12,
+//        // Attribute:Health | Value:+4 | Location:Finger |Range:0
+//        OnionRing = 12,
 
-        // Attribute:Attack | Value:+3 | Location:Necklace |Range:2
-        ButcherKnife = 15,
+//        // Attribute:Attack | Value:+3 | Location:Necklace |Range:2
+//        ButcherKnife = 15,
 
-        // Attribute:Defense | Value:+3 | Location:Necklace |Range:0
-        FriedChicken = 18,
+//        // Attribute:Defense | Value:+3 | Location:Necklace |Range:0
+//        FriedChicken = 18,
 
-        // Attribute:Speed | Value:+5 | Location:Necklace |Range:0
-        ScreamRing = 22,
-    }
+//        // Attribute:Speed | Value:+5 | Location:Necklace |Range:0
+//        ScreamRing = 22,
+//    }
 
-    /// <summary>
-    /// Friendly strings for the Enum Class
-    /// </summary>
-    public static class SpecialDropEnumExtensions
-    {
-        /// <summary>
-        /// Display a String for the Enums
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static string ToMessage(this SpecialDropEnum value)
-        {
-            // Default String
-            var Message = "None";
+//    /// <summary>
+//    /// Friendly strings for the Enum Class
+//    /// </summary>
+//    public static class SpecialDropEnumExtensions
+//    {
+//        /// <summary>
+//        /// Display a String for the Enums
+//        /// </summary>
+//        /// <param name="value"></param>
+//        /// <returns></returns>
+//        public static string ToMessage(this SpecialDropEnum value)
+//        {
+//            // Default String
+//            var Message = "None";
 
-            switch (value)
-            {
-                case SpecialDropEnum.ButcherKnife:
-                    Message = "ButcherKnife";
-                    break;
+//            switch (value)
+//            {
+//                case SpecialDropEnum.ButcherKnife:
+//                    Message = "ButcherKnife";
+//                    break;
 
-                case SpecialDropEnum.FriedChicken:
-                    Message = "FriedChicken";
-                    break;
+//                case SpecialDropEnum.FriedChicken:
+//                    Message = "FriedChicken";
+//                    break;
 
-                case SpecialDropEnum.OnionRing:
-                    Message = "OnionRing";
-                    break;
+//                case SpecialDropEnum.OnionRing:
+//                    Message = "OnionRing";
+//                    break;
 
-                case SpecialDropEnum.Refrigerator:
-                    Message = "Refrigerator";
-                    break;
+//                case SpecialDropEnum.Refrigerator:
+//                    Message = "Refrigerator";
+//                    break;
 
-                case SpecialDropEnum.ScreamRing:
-                    Message = "ScreamRing";
-                    break;
+//                case SpecialDropEnum.ScreamRing:
+//                    Message = "ScreamRing";
+//                    break;
 
-                case SpecialDropEnum.Unknown:
-                default:
-                    break;
-            }
+//                case SpecialDropEnum.Unknown:
+//                default:
+//                    break;
+//            }
 
-            return Message;
-        }
-    }
+//            return Message;
+//        }
+//    }
 
-    /// <summary>
-    /// Helper for the Attribute Enum Class
-    /// </summary>
-    public static class SpecialDropEnumHelper
-    {
-        /// <summary>
-        /// Returns a list of strings of the enum for Attribute
-        /// Removes the attributes that are not changable by Items such as Unknown
-        /// </summary>
-        public static List<string> GetListItem
-        {
-            get
-            {
-                var myList = Enum.GetNames(typeof(SpecialDropEnum)).ToList();
-                return myList;
-            }
-        }
+//    /// <summary>
+//    /// Helper for the Attribute Enum Class
+//    /// </summary>
+//    public static class SpecialDropEnumHelper
+//    {
+//        /// <summary>
+//        /// Returns a list of strings of the enum for Attribute
+//        /// Removes the attributes that are not changable by Items such as Unknown
+//        /// </summary>
+//        public static List<string> GetListItem
+//        {
+//            get
+//            {
+//                var myList = Enum.GetNames(typeof(SpecialDropEnum)).ToList();
+//                return myList;
+//            }
+//        }
 
-        /// <summary>
-        /// Returns a list of strings of the enum for Attribute
-        /// Removes the unknown
-        /// </summary>
-        public static List<string> GetListSpecialDrop
-        {
-            get
-            {
-                var myList = Enum.GetNames(typeof(SpecialDropEnum)).ToList().Where(m => m.ToString().Equals("Unknown") == false).ToList();
-                return myList;
-            }
-        }
+//        /// <summary>
+//        /// Returns a list of strings of the enum for Attribute
+//        /// Removes the unknown
+//        /// </summary>
+//        public static List<string> GetListSpecialDrop
+//        {
+//            get
+//            {
+//                var myList = Enum.GetNames(typeof(SpecialDropEnum)).ToList().Where(m => m.ToString().Equals("Unknown") == false).ToList();
+//                return myList;
+//            }
+//        }
 
-        /// <summary>
-        /// Given the String for an enum, return its value.  That allows for the enums to be numbered 2,4,6 rather than 1,2,3
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static SpecialDropEnum ConvertStringToEnum(string value)
-        {
-            if (value == "None")
-            {
-                return SpecialDropEnum.Unknown;
-            }
+//        /// <summary>
+//        /// Given the String for an enum, return its value.  That allows for the enums to be numbered 2,4,6 rather than 1,2,3
+//        /// </summary>
+//        /// <param name="value"></param>
+//        /// <returns></returns>
+//        public static SpecialDropEnum ConvertStringToEnum(string value)
+//        {
+//            if (value == "None")
+//            {
+//                return SpecialDropEnum.Unknown;
+//            }
 
-            return (SpecialDropEnum)Enum.Parse(typeof(SpecialDropEnum), value);
-        }
-    }
-}
+//            return (SpecialDropEnum)Enum.Parse(typeof(SpecialDropEnum), value);
+//        }
+//    }
+//}
