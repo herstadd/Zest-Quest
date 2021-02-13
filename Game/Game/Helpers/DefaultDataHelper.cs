427ecc973c86f6225ac8b33e867264c7896c09ab
JungBok Cho <chojb2015@gmail.com>
2021-02-13 11:33:37 -0800

    Add DefaultDataHelper.cs to the Helpers folder

---
+++ Game/Game/Helpers/DefaultDataHelper.cs
@@ -0,0 +1,66 @@
+﻿using Game.Models;
+using Game.GameRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Helpers
+{
+    public static class DefaultDataHelper
+    {
+        static List<CharacterModel> CharacterList = null;
+        static List<MonsterModel> MonsterList = null;
+        static List<ItemModel> ItemList = null;
+        static List<ScoreModel> ScoreList = null;
+
+        static DefaultDataHelper()
+        {
+            if(CharacterList == null || MonsterList == null ||
+                ItemList == null || ScoreList == null)
+            {
+                CharacterList = DefaultData.LoadData(new CharacterModel());
+                MonsterList = DefaultData.LoadData(new MonsterModel());
+                ItemList = DefaultData.LoadData(new ItemModel());
+                ScoreList = DefaultData.LoadData(new ScoreModel());
+            }
+        }
+
+        public static CharacterModel GetCharacter(CharacterJobEnum Target)
+        {
+
+            foreach (CharacterModel character in CharacterList)
+            {
+                if (character.Job == Target)
+                {
+                    return character;
+                }
+            }
+            return null;
+        }
+
+        public static MonsterModel GetMonster(MonsterTypeEnum Target)
+        {
+            foreach (MonsterModel monster in MonsterList)
+            {
+                if (monster.MonsterType == Target)
+                {
+                    return monster;
+                }
+            }
+            return null;
+        }
+
+        public static ItemModel GetCharacter(ItemModelEnum Target)
+        {
+            foreach (ItemModel item in ItemList)
+            {
+                if (item.Type == Target)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
