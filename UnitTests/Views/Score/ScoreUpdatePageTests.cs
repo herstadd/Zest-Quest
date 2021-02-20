3513ba042276a06c03fea7971ca5a19b1fb4b00a
Sam Borhan <samborhan1566@gmail.com>
2021-02-20 09:23:23 -0800

    Added  ScoreUpdatePage_Save_Clicked_Null_Name_Should_Pass() method in ScoreUpdatePageTests.cs

--- UnitTests/Views/Score/ScoreUpdatePageTests.cs
+++ UnitTests/Views/Score/ScoreUpdatePageTests.cs
@@ -101,6 +101,21 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void ScoreUpdatePage_Save_Clicked_Null_Name_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = null;
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void ScoreUpdatePage_OnBackButtonPressed_Valid_Should_Pass()
         {
             // Arrange
