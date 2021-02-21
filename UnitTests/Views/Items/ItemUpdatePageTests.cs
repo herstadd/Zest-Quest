94f9c5a59526eb6f505305eb0d83480d3f0c7247
Sam Borhan <samborhan1566@gmail.com>
2021-02-20 19:45:42 -0800

    Add  ItemUpdatePage_Type_Changed_Default_Should_Pass() method to ItemUpdatePageTests.cs

--- UnitTests/Views/Items/ItemUpdatePageTests.cs
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -86,6 +86,20 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void ItemUpdatePage_Type_Changed_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Type_Changed(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void ItemUpdatePage_Save_Clicked_Null_Image_Should_Pass()
         {
             // Arrange
