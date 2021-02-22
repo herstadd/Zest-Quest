0f48409e021b44c5fae4e52a0c0e7bb3c5abe08d
Ruifeng Wang <rwang@seattleu.edu>
2021-02-21 22:56:32 -0800

    Add CharacterCreatePage_ClickedOnItem_OffHand_Should_Pass() to CharacterCreatePageTests.cs

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -473,6 +473,24 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterCreatePage_ClickedOnItem_OffHand_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.BindingContext = "OffHand";
+
+            var NewPage = new CharacterCreatePage();
+
+            // Act
+            NewPage.ClickedOnItem(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
         //[Test]
         //public void CharacterCreatePage_RollDice_Clicked_Default_Should_Pass()
         //{
