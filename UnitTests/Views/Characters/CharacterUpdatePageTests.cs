ec3a2c6b6cd29e3816335359b8904a0b47016bd6
herstadd <herstadd@seattleu.edu>
2021-02-22 01:20:38 -0800

    Added CharacterUpdatePage_ClickedOnItem_Feet_Should_Pass UT to CharacterUpdatePageTests.cs

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -467,6 +467,27 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterUpdatePage_ClickedOnItem_Feet_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            //button.Text = "Confirm";
+            button.BindingContext = "Feet";
+
+            var data = new CharacterModel();
+            var ViewModel = new GenericViewModel<CharacterModel>(data);
+            var NewPage = new CharacterUpdatePage(ViewModel);
+
+            // Act
+            NewPage.ClickedOnItem(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
 
         //[Test]
         //public void CharacterUpdatePage_RollDice_Clicked_Default_Should_Pass()
