565d0b90b74a9e61dcddd062349162150d37ba95
herstadd <herstadd@seattleu.edu>
2021-02-22 00:58:51 -0800

    Added CharacterUpdatePage_ClickedOnItemSelection_PrimaryHand_Should_Pass UT to CharacterUpdatePageTests.cs

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -257,6 +257,27 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterUpdatePage_ClickedOnItemSelection_PrimaryHand_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.Text = "Confirm";
+            button.BindingContext = "PrimaryHand";
+
+            var data = new CharacterModel();
+            var ViewModel = new GenericViewModel<CharacterModel>(data);
+            var NewPage = new CharacterUpdatePage(ViewModel);
+
+            // Act
+            NewPage.ClickedOnItemSelection(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
         //[Test]
         //public void CharacterUpdatePage_RollDice_Clicked_Default_Should_Pass()
         //{
