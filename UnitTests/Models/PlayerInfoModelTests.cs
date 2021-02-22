db05d60e12a5e4d8e5d4b89cd7b0dbc50081e82f
herstadd <herstadd@seattleu.edu>
2021-02-22 14:14:34 -0800

    Added PlayerInfoModel_Constructor_Character_SchoolChef_Should_Pass UT to PlayerInfoModelTests.cs

--- UnitTests/Models/PlayerInfoModelTests.cs
+++ UnitTests/Models/PlayerInfoModelTests.cs
@@ -127,6 +127,21 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_SchoolChef_Should_Pass()
+        {
+            // Arrange
+            var data = new CharacterModel { Job = CharacterJobEnum.SchoolChef };
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
         //[Test]
         //public void PlayerInfoModel_IsAbilityAvailable_Available_Should_Pass()
         //{
