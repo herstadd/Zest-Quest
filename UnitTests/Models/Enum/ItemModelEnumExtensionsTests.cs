8f4a7a8978287eea24dc6de22f8ae7bffe0963da
JungBok Cho <chojb2015@gmail.com>
2021-02-22 02:37:10 -0800

    Add ItemModelEnumExtensionsTests.cs to file

---
+++ UnitTests/Models/Enum/ItemModelEnumExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelEnumExtensionsTests
+    {
+        [Test]
+        public void ItemModelEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemLocationEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Unknown", result);
+        }
+    }
+}
