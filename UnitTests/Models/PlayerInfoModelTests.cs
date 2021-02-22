7833b808ece05fa9282498c840b8b46d202fccf3
herstadd <herstadd@seattleu.edu>
2021-02-22 14:15:11 -0800

    Added PlayerInfoModel_Constructor_Character_SushiChef_Should_Pass UT to PlayerInfoModelTests.cs

--- UnitTests/Models/PlayerInfoModelTests.cs
+++ UnitTests/Models/PlayerInfoModelTests.cs
@@ -142,6 +142,21 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_SushiChef_Should_Pass()
+        {
+            // Arrange
+            var data = new CharacterModel { Job = CharacterJobEnum.SushiChef };
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
         //[Test]
         //public void PlayerInfoModel_IsAbilityAvailable_Available_Should_Pass()
         //{
