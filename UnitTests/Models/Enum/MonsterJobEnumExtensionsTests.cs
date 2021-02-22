6472ff19796b72dd1c2676af726809076b4bf2bc
JungBok Cho <chojb2015@gmail.com>
2021-02-22 02:15:30 -0800

    Add MonsterJobEnumExtensionsTests.cs file to project

---
+++ UnitTests/Models/Enum/MonsterJobEnumExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class MonsterJobEnumExtensionsTests
+    {
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterTypeEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Player", result);
+        }
+    }
+}
