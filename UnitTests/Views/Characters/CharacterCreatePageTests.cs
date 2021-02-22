02f8abe708d96807efc98a28394f3f1651e968ea
Ruifeng Wang <rwang@seattleu.edu>
2021-02-21 22:54:35 -0800

    Add CharacterCreatePage_ClickedOnItem_Head_Should_Pass() to CharacterCreatePageTests.cs

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -419,6 +419,24 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterCreatePage_ClickedOnItem_Head_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.BindingContext = "Head";
+
+            var NewPage = new CharacterCreatePage();
+
+            // Act
+            NewPage.ClickedOnItem(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
         //[Test]
         //public void CharacterCreatePage_RollDice_Clicked_Default_Should_Pass()
         //{
