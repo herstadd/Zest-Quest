186cd49f5888a0b5f9bb49c2a5658fcb09d3ca92
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 02:20:51 -0800

    Fix CharacterModel_GetSpeed_Default_Should_Pass in CharacterModelTests.cs

--- UnitTests/Models/CharacterModelTests.cs
+++ UnitTests/Models/CharacterModelTests.cs
@@ -355,7 +355,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual(1, result);
+            Assert.AreEqual(4, result);
         }
 
         [Test]
