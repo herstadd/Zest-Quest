f5d9fe042b8d1e94075441697d66b51d66044360
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 15:08:07 -0800

    Add ButtonClickedHelperTests.cs

---
+++ UnitTests/Helpers/ButtonClickedHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.Helpers;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class ButtonClickedHelperTests
+    {
+        [Test]
+        public void ButtonClicked_Minus_IsNotMaxHealth_Should_Return_2()
+        {
+            // Arrange
+
+            // Act
+            var result = ButtonClickedHelper.ValueChange("-", 3, false);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
