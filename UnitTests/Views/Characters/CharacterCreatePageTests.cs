07b6db4fe358765315ae204e1e7bb3949a528e73
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 00:18:41 -0800

    Add CharacterCreatePage_AddItemButtonClicked_Selected_RingPop_Should_Pass to CharacterCreatePageTests.cs

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -677,6 +677,25 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterCreatePage_AddItemButtonClicked_Selected_RingPop_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.BindingContext = "RingPop";
+
+            var NewPage = new CharacterCreatePage();
+
+
+            // Act
+            NewPage.AddItemButtonClicked(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
         //[Test]
         //public void CharacterCreatePage_RollDice_Clicked_Default_Should_Pass()
         //{
