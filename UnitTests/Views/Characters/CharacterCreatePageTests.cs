3a2e391718759a0256fde0631d0f03b1d4b440c1
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 00:17:02 -0800

    Add CharacterCreatePage_AddItemButtonClicked_Selected_Knife_Should_Pass to CharacterCreatePageTests.cs

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -639,6 +639,25 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterCreatePage_AddItemButtonClicked_Selected_Knife_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.BindingContext = "Knife";
+
+            var NewPage = new CharacterCreatePage();
+
+
+            // Act
+            NewPage.AddItemButtonClicked(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
         //[Test]
         //public void CharacterCreatePage_RollDice_Clicked_Default_Should_Pass()
         //{
