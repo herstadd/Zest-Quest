7d853a5f51eec91eb9ebb40558f4ee4542701dc8
herstadd <herstadd@seattleu.edu>
2021-02-22 01:02:29 -0800

    Added UT CharacterUpdatePage_ClickedOnItemSelection_RightFinger_Should_Pass to CharacterUpdatePageTests.cs

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -320,6 +320,28 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterUpdatePage_ClickedOnItemSelection_RightFinger_Should_Pass()
+        {
+            // Arrange
+            Button button = new Button();
+            button.Text = "Confirm";
+            button.BindingContext = "RightFinger";
+
+            var data = new CharacterModel();
+            var ViewModel = new GenericViewModel<CharacterModel>(data);
+            var NewPage = new CharacterUpdatePage(ViewModel);
+
+            // Act
+            NewPage.ClickedOnItemSelection(button, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
         //[Test]
         //public void CharacterUpdatePage_RollDice_Clicked_Default_Should_Pass()
         //{
