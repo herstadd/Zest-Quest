5b5b2b23e3ca38892ccc822dd84f57f97a4513a8
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 17:43:17 -0800

    Add CharacterUpdatePage_PopulateInitialItems_Null_PrimaryHand_Item_Should_Pass to CharacterUpdatePageTests.cs

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -802,6 +802,22 @@ namespace UnitTests.Views
             Assert.IsTrue(true);
         }
 
+        [Test]
+        public void CharacterUpdatePage_PopulateInitialItems_Null_PrimaryHand_Item_Should_Pass()
+        {
+            // Arrange
+            var data = new CharacterModel();
+            data.PrimaryHand = null;
+
+            // Act
+            page.PopulateInitialItems("PrimaryHand", data);
+
+            // Reset 
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
 
         //[Test]
         //public void CharacterUpdatePage_RollDice_Clicked_Default_Should_Pass()
