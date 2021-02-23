331a3fe7a466add17b72b4b91d3daf26103a10f5
Sam Borhan <samborhan1566@gmail.com>
2021-02-22 18:32:29 -0800

    Add DefaultDataHelper_GetCharacter_Unkown_CharacterJobEnum_Should_Return_Null() method in DefaultDataHelperTests.cs

--- UnitTests/Helpers/DefaultDataHelperTests.cs
+++ UnitTests/Helpers/DefaultDataHelperTests.cs
@@ -1,13 +1,27 @@
 ﻿using NUnit.Framework;
 
 using Game.Helpers;
+using Game.GameRules;
+using Game.Models;
 
 namespace UnitTests.Helpers
 {
     [TestFixture]
     public class DefaultDataHelperTests
     {
+        [Test]
+        public void DefaultDataHelper_GetCharacter_Unkown_CharacterJobEnum_Should_Return_Null()
+        {
+            // Arrange
 
+            // Act
+            var Result = DefaultDataHelper.GetCharacter(CharacterJobEnum.Unknown);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(null, Result);
+        }
 
     }
 }
