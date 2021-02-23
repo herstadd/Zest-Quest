c1fb9a13894d189ac28b644cce4e1142ead4a74e
Ruifeng Wang <rwang@seattleu.edu>
2021-02-22 17:44:57 -0800

    Add CharacterUpdatePage_PopulateInitialItems_Valid_OffHand_Item_Should_Pass to CharacterUpdatePageTests.cs

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -850,6 +850,22 @@ namespace UnitTests.Views
             Assert.IsTrue(true);
         }
 
+        [Test]
+        public void CharacterUpdatePage_PopulateInitialItems_Valid_OffHand_Item_Should_Pass()
+        {
+            // Arrange
+            var data = new CharacterModel();
+            data.OffHand = "Knife";
+
+            // Act
+            page.PopulateInitialItems("OffHand", data);
+
+            // Reset 
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
 
         //[Test]
         //public void CharacterUpdatePage_RollDice_Clicked_Default_Should_Pass()
