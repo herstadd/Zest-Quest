3f334ad88471bbb1a0bc31b07d3598c105f9d0bb
JungBok Cho <chojb2015@gmail.com>
2021-02-26 12:47:10 -0800

    Add MonsterClassEnumHelperHelper_GetListMonsterClass__Valid_Should_Pass in MonsterClassEnumHelperTests.cs

--- UnitTests/Helpers/MonsterClassEnumHelperTests.cs
+++ UnitTests/Helpers/MonsterClassEnumHelperTests.cs
@@ -8,7 +8,19 @@ namespace UnitTests.Helpers
 {
     [TestFixture]
     class MonsterClassEnumHelperTests
-    { 
-        
+    {
+        [Test]
+        public void MonsterClassEnumHelperHelper_GetListMonsterClass__Valid_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterClassEnumHelper.GetListMonsterClass;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+        }
     }
 }
