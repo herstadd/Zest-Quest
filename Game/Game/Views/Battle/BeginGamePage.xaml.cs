67ba2291a52a88bdf49293b47cf33d5ed3e047ca
Sam Borhan <samborhan1566@gmail.com>
2021-02-26 12:06:00 -0800

    Add BeginGamePage File located in Views/Battle Folder to link with Kitchen Button at GamePage in Game.csproj

---
+++ Game/Game/Views/Battle/BeginGamePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Begin Game Page
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BeginGamePage : ContentPage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BeginGamePage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+		/// Jump to the Pick Characters Page
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public async void LunchRushButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PickCharactersPage());
+        }
+    }
+}
--- Game/Game/Views/Home/GamePage.xaml.cs
+++ Game/Game/Views/Home/GamePage.xaml.cs
@@ -25,7 +25,7 @@ namespace Game.Views
 		/// <param name="e"></param>
         public async void DungeonButton_Clicked(object sender, EventArgs e)
         {
-			await Navigation.PushAsync(new PickCharactersPage());
+			await Navigation.PushAsync(new BeginGamePage());
 		}
 
 		/// <summary>
