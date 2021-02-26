57732b366849e910367160bd27e3708e6387031e
JungBok Cho <chojb2015@gmail.com>
2021-02-26 12:38:42 -0800

    Add MonsterClassEnumExtensionsTests_ToMessage_Standard_Default_Should_Pass in MonsterClassEnumExtensionsTests.cs

--- UnitTests/Models/Enum/MonsterClassEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/MonsterClassEnumExtensionsTests.cs
@@ -7,8 +7,20 @@ namespace UnitTests.Models
     [TestFixture]
     public class MonsterClassEnumExtensionsTests
     {
-        
+        [Test]
+        public void MonsterClassEnumExtensionsTests_ToMessage_Standard_Default_Should_Pass()
+        {
+            // Arrange
 
-        
+            // Act
+            var result = MonsterClassEnumExtensions.ToMessage(MonsterClassEnum.Standard);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Standard", result);
+        }
+
+
     }
 }
