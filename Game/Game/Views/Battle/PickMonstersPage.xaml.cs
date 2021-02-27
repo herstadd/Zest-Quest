45f61fdc0af4bd433ce09fc0f5225ca3501925dc
Sam Borhan <samborhan1566@gmail.com>
2021-02-26 21:13:16 -0800

    Add PickMonsterPage file at Views\Battle Folder in Game.csproj

---
+++ Game/Game/Views/Battle/PickMonstersPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickMonstersPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly CharacterIndexViewModel ViewModel = CharacterIndexViewModel.Instance;
+
+        // Empty Constructor for UTs
+        public PickMonstersPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
+        public PickMonstersPage()
+        {
+            InitializeComponent();
+
+            BindingContext = BattleEngineViewModel.Instance.PartyCharacterList;
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// Jump to the Pick Characters Page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PickCharactersPage());
+        }
+
+        /// <summary>
+		/// Jump to the Battle game Page
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public async void RoundStartBUtton_CLicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new BattlePage()));
+            await Navigation.PopAsync();
+        }
+    }
+}
