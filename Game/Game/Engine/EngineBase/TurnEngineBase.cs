b58ee898218ca5902b4cee5cb5a2b210d79e13e1
herstadd <herstadd@seattleu.edu>
2021-03-04 19:53:11 -0800

    Undoing Mike's changes in TurnEngineBase.cs

--- Game/Game/Engine/EngineBase/TurnEngineBase.cs
+++ Game/Game/Engine/EngineBase/TurnEngineBase.cs
@@ -38,7 +38,7 @@ namespace Game.Engine.EngineBase
         #endregion Algrorithm
 
         // The Turn Engine
-        //public ITurnEngineInterface Turn = null;
+        // public ITurnEngineInterface Turn = null;
 
         // Hold the BaseEngine
         public EngineSettingsModel EngineSettings = EngineSettingsModel.Instance;
@@ -482,12 +482,10 @@ namespace Game.Engine.EngineBase
         /// Apply the Damage to the Target
         /// </summary>
         /// <param name="Target"></param>
-        public virtual int ApplyDamage(PlayerInfoModel Target)
+        public virtual void ApplyDamage(PlayerInfoModel Target)
         {
             Target.TakeDamage(EngineSettings.BattleMessagesModel.DamageAmount);
             EngineSettings.BattleMessagesModel.CurrentHealth = Target.GetCurrentHealthTotal;
-
-            return EngineSettings.BattleMessagesModel.DamageAmount;
         }
 
         /// <summary>
@@ -664,10 +662,8 @@ namespace Game.Engine.EngineBase
         /// <returns></returns>
         public virtual HitStatusEnum RollToHitTarget(int AttackScore, int DefenseScore)
         {
-            // Roll a 20 sided dice
             var d20 = DiceHelper.RollDice(1, 20);
 
-            // if dice roll is 1, automatic miss
             if (d20 == 1)
             {
                 EngineSettings.BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
@@ -682,7 +678,6 @@ namespace Game.Engine.EngineBase
                 return EngineSettings.BattleMessagesModel.HitStatus;
             }
 
-            // if dice is 20, automatic hit
             if (d20 == 20)
             {
                 EngineSettings.BattleMessagesModel.AttackStatus = " rolls 20 for hit ";
@@ -696,7 +691,6 @@ namespace Game.Engine.EngineBase
                 return EngineSettings.BattleMessagesModel.HitStatus;
             }
 
-            // if hit score is less than defense, it's a miss
             var ToHitScore = d20 + AttackScore;
             if (ToHitScore < DefenseScore)
             {
