950ff68fda3bfb10e4f57bb29c2eba83daa48b34
Ruifeng Wang <rwang@seattleu.edu>
2021-03-05 21:00:33 -0800

    Add BeginGamePageTests.cs to project

---
+++ UnitTests/Views/Battle/BeginGamePageTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+using Game;
+using Game.Views;
+using Game.Models;
+using Game.ViewModels;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class BeginGamePageTests
+    {
+        App app;
+        BeginGamePage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            // For now, set the engine to the Koenig Engine, change when ready 
+            BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
+
+            page = new BeginGamePage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void BeginGamePage_LunchRushButton_Clicked_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.LunchRushButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
