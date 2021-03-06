da67a0274f6abb24c64c9dbf3641dadcf5afc030
herstadd <herstadd@seattleu.edu>
2021-03-06 04:20:43 -0800

    Updated UT TurnEngine_DropItems_Valid_Monster_Items_0_Random_Drop_1_Should_Return_2 in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -688,7 +688,7 @@ namespace UnitTests.Engine.EngineGame
         }
 
         [Test]
-        public void TurnEngine_DropItems_Valid_Monster_Items_0_Random_Drop_1_Should_Return_1()
+        public void TurnEngine_DropItems_Valid_Monster_Items_0_Random_Drop_1_Should_Return_2()
         {
             // Arrange
             var player = new CharacterModel();
@@ -707,7 +707,7 @@ namespace UnitTests.Engine.EngineGame
             DiceHelper.DisableForcedRolls();
 
             // Assert
-            Assert.AreEqual(1, result);
+            Assert.AreEqual(2, result);
         }
         #endregion DropItems
 
