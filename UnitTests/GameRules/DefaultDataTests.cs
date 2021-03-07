3f5caeff0e227a792af773a8d1da3e66c1bbe4e0
herstadd <herstadd@seattleu.edu>
2021-03-07 01:57:35 -0800

    Updated UT DefaultData_Valid_LoadData_Item_Should_Pass in DefaultDataTests.cs

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -21,7 +21,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(17, result.Count());
+            Assert.AreEqual(25, result.Count());
         }
 
         [Test]
