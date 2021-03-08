2e6091442307ded3b2294c723804209f698ed1b3
herstadd <herstadd@seattleu.edu>
2021-03-08 05:13:26 -0800

    Cleaned up UT TurnEngine_TurnAsAttack_Valid_Character_Attacks_Monster_Levels_Up_Should_Pass to remove old character and monster in TurnEngineKoenigTests.cs

--- UnitTests/Engine/EngineKoenig/TurnEngineKoenigTests.cs
+++ UnitTests/Engine/EngineKoenig/TurnEngineKoenigTests.cs
@@ -1068,6 +1068,8 @@ namespace UnitTests.Engine.EngineKoenig
 
             // Reset
             DiceHelper.DisableForcedRolls();
+            Engine.EngineSettings.CharacterList.Remove(CharacterPlayer);
+            Engine.EngineSettings.MonsterList.Remove(MonsterPlayer);
 
             // Assert
             Assert.AreEqual(true, result);
