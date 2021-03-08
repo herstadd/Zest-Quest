ce5bef438011de58a96e50218816aa758aded571
JungBok Cho <chojb2015@gmail.com>
2021-03-08 00:17:48 -0800

    Add MapModel_ReturnNextEmptyLocation_Character_MoveOnlyOne_Valid_Should_Pass in MapModelTests.cs

--- UnitTests/Models/MapModelTests.cs
+++ UnitTests/Models/MapModelTests.cs
@@ -1168,6 +1168,51 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void MapModel_ReturnNextEmptyLocation_Character_MoveOnlyOne_Valid_Should_Pass()
+        {
+            // Arrange
+            var map = new MapModel();
+
+            map.MapXAxiesCount = 6;
+            map.MapYAxiesCount = 6;
+            map.MapGridLocation = new MapModelLocation[map.MapXAxiesCount, map.MapYAxiesCount];
+
+            var PlayerList = new List<PlayerInfoModel>();
+
+            var Character = new CharacterModel();
+            var CharacterPlayer = new PlayerInfoModel(Character);
+            PlayerList.Add(CharacterPlayer);
+
+            var Monster = new MonsterModel();
+            PlayerList.Add(new PlayerInfoModel(Monster));
+
+            map.PopulateMapModel(PlayerList);
+            MapModelLocation playerNext = new MapModelLocation();
+            playerNext.Row = 0;
+            playerNext.Column = 0;
+
+            MapModelLocation monsterNext = new MapModelLocation();
+            monsterNext.Row = 0;
+            monsterNext.Column = 5;
+
+            var currPlayer = map.GetLocationForPlayer(CharacterPlayer);
+            var currMonster = map.GetLocationForPlayer(CharacterPlayer);
+
+            map.MovePlayerOnMap(currPlayer, playerNext);
+            map.MovePlayerOnMap(currMonster, monsterNext);
+
+            // Act
+            var result = map.ReturnNextEmptyLocation(monsterNext, playerNext, CharacterJobEnum.HeadChef);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(1, result.Column);
+            Assert.AreEqual(0, result.Row);
+        }
+
+
+        [Test]
         public void MapModel_ReturnNextEmptyLocation_SousChef_Should_Return_0()
         {
             // Arrange
