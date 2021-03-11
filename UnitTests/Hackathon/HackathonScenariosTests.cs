b36632545a842520790d5897d7c92d4f7e90ca21
Ruifeng Wang <rwang@seattleu.edu>
2021-03-10 20:58:41 -0800

    Add HackathonScenario_10_Player_Die_Should_Revive_Not_Revive_Second_Time_Return_Damage_Amount to HackathonScenariosTests.cs

--- UnitTests/Hackathon/HackathonScenariosTests.cs
+++ UnitTests/Hackathon/HackathonScenariosTests.cs
@@ -311,7 +311,7 @@ namespace Scenario
 
         #region Scenario10
         [Test]
-        public void HackathonScenario_10_Player_Critical_Hit_Should_Return_Negative_1()
+        public void HackathonScenario_10_Player_Die_Should_Revive_Max_Health()
         {
             /* 
             * Scenario Number:  
@@ -364,6 +364,8 @@ namespace Scenario
                                 PlayerType = PlayerTypeEnum.Character,
                             });
 
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayerMike);
+
             // Allow critical hit
             EngineViewModel.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit = true;
 
@@ -387,6 +389,90 @@ namespace Scenario
             //Assert
             Assert.AreEqual(-1, result);
             Assert.AreEqual(10, CharacterPlayerMike.CurrentHealth);
+        }
+
+        public void HackathonScenario_10_Player_Die_Should_Revive_Not_Revive_Second_Time_Return_Damage_Amount()
+        {
+            /* 
+            * Scenario Number:  
+            *      10
+            *      
+            * Description: 
+            *      Make a character who will get hit and die then check if they revive with CurrentHealth == MaxHealth
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      Add class variable to BasePlayerModel.cs
+            *      Change ApplyDamage in TurnEngine.cs
+            * 
+            * Test Algrorithm:
+            *      Create Character
+            *      Set Max health to 10
+            *      Set Current Health to 1 so he is weak
+            *      Set SavedByMax to true
+            *      Have the character take 2 damage so they would die
+            *      
+            * 
+            * Test Conditions:
+            *      Default condition is sufficient
+            * 
+            * Validation:
+            *      Verify damage check returned 2
+            *      Verify CurrentHealth is 0
+            *      Verify character is removed
+            *  
+            */
+
+            //Arrange
+
+            Game.Engine.EngineGame.TurnEngine TurnEngine = new Game.Engine.EngineGame.TurnEngine();
+            DiceHelper.EnableForcedRolls();
+            DiceHelper.SetForcedRollValue(20);
+
+            // Set Character Conditions
+
+            EngineViewModel.Engine.EngineSettings.MaxNumberPartyCharacters = 1;
+
+            var CharacterPlayerMike = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = 100, // Will go first...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                MaxHealth = 10,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Mike",
+                                Job = CharacterJobEnum.HeadChef,
+                                PlayerType = PlayerTypeEnum.Character,
+                                SavedByMax = true,
+                            });
+
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayerMike);
+
+            // Allow critical hit
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit = true;
+
+            // Set Monster Conditions
+
+            // Auto Battle will add the monsters
+
+            // Monsters always hit
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Hit;
+            EngineViewModel.Engine.EngineSettings.BattleMessagesModel.DamageAmount = 2;
+
+            //Act
+            var result = TurnEngine.ApplyDamage(CharacterPlayerMike);
+
+            //Reset
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit = false;
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
+            EngineViewModel.Engine.EngineSettings.BattleMessagesModel.DamageAmount = 0;
+            DiceHelper.DisableForcedRolls();
+
+            //Assert
+            Assert.AreEqual(2, result);
+            Assert.AreEqual(0, CharacterPlayerMike.CurrentHealth);
+            Assert.AreEqual(null, EngineViewModel.Engine.EngineSettings.PlayerList.Find(m => m.Name.Equals("Mike")));
         }
         #endregion Scenario10
 
