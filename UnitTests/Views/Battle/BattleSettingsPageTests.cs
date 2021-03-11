c67b31038befa8893aca9b0bcd8aa606638ae9e0
Ruifeng Wang <rwang@seattleu.edu>
2021-03-10 23:23:31 -0800

    Add BattleSettingsPage_EnableSleeplessZombies_Toggled_Default_Should_Pass to BattleSettingsPageTests.cs

--- UnitTests/Views/Battle/BattleSettingsPageTests.cs
+++ UnitTests/Views/Battle/BattleSettingsPageTests.cs
@@ -196,6 +196,25 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void BattleSettingsPage_EnableSleeplessZombies_Toggled_Default_Should_Pass()
+        {
+            // Arrange
+
+            var control = (Switch)page.FindByName("EnableSleeplessZombiesSwitch");
+            var current = control.IsToggled;
+
+            ToggledEventArgs args = new ToggledEventArgs(current);
+
+            // Act
+            page.EnableSleeplessZombiesSwitch_Toggled(null, args);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(!current); // Got to here, so it happened...
+        }
+
+        [Test]
         public void BattleSettingsPage_AllowCriticalMiss_Toggled_Default_Should_Pass()
         {
             // Arrange
