2c43fecb8d290ef1bb2c45fa804ce073f66f90ac
JungBok Cho <chojb2015@gmail.com>
2021-03-14 01:19:23 -0800

    Remove unnecessary test and modify ItemLocationEnumHelper_GetListCharacter_Should_Pass function in ItemLocationEnumHelperTests.cs

--- UnitTests/Helpers/ItemLocationEnumHelperTests.cs
+++ UnitTests/Helpers/ItemLocationEnumHelperTests.cs
@@ -5,27 +5,13 @@ using NUnit.Framework;
 
 using Game.Models;
 using Game.Helpers;
+using System.Collections.Generic;
 
 namespace UnitTests.Helpers
 {
     [TestFixture]
     public class ItemLocationEnumHelperTests
     {
-
-        [Test]
-        public void ItemLocationEnumHelper_ItemLocationEnum_Valid_1Time_6sided_Should_Between_1_and_6()
-        {
-            // Arrange
-
-            // Act
-            var result = ItemLocationEnumHelper.GetListItem;
-
-            // Reset
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
         [Test]
         public void ItemLocationEnumHelper_GetListCharacter_Should_Pass()
         {
@@ -35,13 +21,14 @@ namespace UnitTests.Helpers
             var myDataList = ItemLocationEnumHelper.GetListCharacter;
 
             // Get Expected set
-            var myList = Enum.GetNames(typeof(ItemLocationEnum)).ToList();
-            var myExpectedList = myList.Where(a =>
-                                          a.ToString() != ItemLocationEnum.Unknown.ToString() &&
-                                           a.ToString() != ItemLocationEnum.Finger.ToString()
-                                            )
-                                            .OrderBy(a => a)
-                                            .ToList();
+            var myExpectedList = new List<string>();
+            foreach (ItemLocationEnum item in Enum.GetValues(typeof(ItemLocationEnum)))
+            {
+                if (item != ItemLocationEnum.Unknown &&
+                    item != ItemLocationEnum.Finger
+                )
+                    myExpectedList.Add(item.ToMessage());
+            }
 
             // Act
 
@@ -79,71 +66,12 @@ namespace UnitTests.Helpers
                 // Assert
                 Assert.AreEqual(true, found, "expected : " + expected + TestContext.CurrentContext.Test.Name);
             }
-
-        }
-
-        [Test]
-        public void ItemLocationEnumHelper_GetListItem_Should_Pass()
-        {
-            // Arrange
-
-            // Instantiate a new ItemLocation Base, should have default of 1 for all values
-            var myDataList = ItemLocationEnumHelper.GetListItem;
-
-            // Get Expected set
-            var myList = Enum.GetNames(typeof(ItemLocationEnum)).ToList();
-            var myExpectedList = myList.Where(a =>
-                                            a.ToString() != ItemLocationEnum.Unknown.ToString() &&
-                                            a.ToString() != ItemLocationEnum.LeftFinger.ToString() &&
-                                            a.ToString() != ItemLocationEnum.RightFinger.ToString()
-                                            )
-                                            .OrderBy(a => a)
-                                            .ToList();
-
-            // Act
-
-            // Make sure each item is in the list
-            foreach (var item in myDataList)
-            {
-                var found = false;
-                foreach (var expected in myExpectedList)
-                {
-                    if (item == expected)
-                    {
-                        found = true;
-                        break;
-                    }
-                }
-
-                // Assert
-                Assert.AreEqual(true, found, "item : " + item + TestContext.CurrentContext.Test.Name);
-            }
-
-            // reverse it, to make sure the list has each item
-            // Make sure each item is in the list
-            foreach (var expected in myExpectedList)
-            {
-                var found = false;
-                {
-                    foreach (var item in myDataList)
-                        if (item == expected)
-                        {
-                            found = true;
-                            break;
-                        }
-                }
-
-                // Assert
-                Assert.AreEqual(true, found, "expected : " + expected + TestContext.CurrentContext.Test.Name);
-            }
-
         }
 
         [Test]
         public void ItemLocationEnumHelper_ConvertStringToEnum_Should_Pass()
         {
             // Arrange
-
             var myList = Enum.GetNames(typeof(ItemLocationEnum)).ToList();
 
             ItemLocationEnum myActual;
