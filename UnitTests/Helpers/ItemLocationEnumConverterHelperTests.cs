7141772e58620812875ccfd8adc5b4985b277c70
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:09:31 -0700

    Add ItemLocationEnumConverterHelperTests_Convert_Valid_Should_Pass to ItemLocationEnumConverterHelperTests.cs

--- UnitTests/Helpers/ItemLocationEnumConverterHelperTests.cs
+++ UnitTests/Helpers/ItemLocationEnumConverterHelperTests.cs
@@ -10,5 +10,23 @@ namespace UnitTests.Helpers
     [TestFixture]
     class ItemLocationEnumConverterHelperTests
     {
+        [Test]
+        public void ItemLocationEnumConverterHelperTests_Convert_Valid_Should_Pass()
+        {
+            // Arrange
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = "PrimaryHand";
+            var Expected = "Primary Hand";
+
+            // Act
+            var Result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
     }
 }
