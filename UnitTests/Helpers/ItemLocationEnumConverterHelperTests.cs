e80cd0f4faea0105b5c7b38d8340a9793de03601
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:10:02 -0700

    Add ItemLocationEnumConverterHelperTests_ConvertBack_Num__Valid_Should_Pass to ItemLocationEnumConverterHelperTests.cs

--- UnitTests/Helpers/ItemLocationEnumConverterHelperTests.cs
+++ UnitTests/Helpers/ItemLocationEnumConverterHelperTests.cs
@@ -45,6 +45,21 @@ namespace UnitTests.Helpers
             Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
         }
 
+        [Test]
+        public void ItemLocationEnumConverterHelperTests_ConvertBack_Num__Valid_Should_Pass()
+        {
+            // Arrange
+            var myConverter = new ItemLocationEnumConverter();
+            var myObject = 12;
+            var Expected = "Necklass";
 
+            // Act
+            var Result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
     }
 }
