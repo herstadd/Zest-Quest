1c16eaf2708ff0b1737ed581de078bfc5f4b255f
JungBok Cho <chojb2015@gmail.com>
2021-03-14 23:52:54 -0700

    Add ItemAttributeEnumConverterHelperTests_Convert__Valid_Should_Pass in ItemAttributeEnumConverterHelperTests.cs

--- UnitTests/Helpers/ItemAttributeEnumConverterHelperTests.cs
+++ UnitTests/Helpers/ItemAttributeEnumConverterHelperTests.cs
@@ -10,6 +10,22 @@ namespace UnitTests.Helpers
     [TestFixture]
     class ItemAttributeEnumConverterHelperTests
     {
-        
+        [Test]
+        public void ItemAttributeEnumConverterHelperTests_Convert__Valid_Should_Pass()
+        {
+            // Arrange
+            var myConverter = new ItemAttributeEnumConverter();
+
+            var myObject = "CurrentHealth";
+            var Expected = "Current Health";
+
+            // Act
+            var Result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
     }
 }
