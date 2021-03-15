a082af1489a688fbf07ed381eb4d586e1e7411fe
Ruifeng Wang <rwang@seattleu.edu>
2021-03-14 22:23:27 -0700

    Add RoundOverPage_ShowPopup_Null_LeftFinger_Item_Should_Return_False to RoundOverPageTests.cs

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -573,5 +573,29 @@ namespace UnitTests.Views
             // Assert
             Assert.AreEqual(true, result);
         }
+
+        [Test]
+        public void RoundOverPage_ShowPopup_Null_LeftFinger_Item_Should_Return_False()
+        {
+            // Arrange
+            RoundOverPage NewPage = new RoundOverPage();
+            NewPage.SelectedCharacter = new PlayerInfoModel
+            {
+                LeftFinger = "Null",
+            };
+
+            ItemModel TestItem = new ItemModel
+            {
+                Location = ItemLocationEnum.LeftFinger,
+            };
+
+            // Act
+            var result = NewPage.ShowPopup(TestItem);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
     }
 }
