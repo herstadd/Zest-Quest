13e62423772ad904dc0d085228c45cf6e48a76de
JungBok Cho <chojb2015@gmail.com>
2021-03-14 23:24:50 -0700

    Add CharacterJobEnumConverterHelperTests_ConvertBack__Valid_Should_Pass to CharacterJobEnumConverterHelperTests.cs

--- UnitTests/Helpers/CharacterJobEnumConverterHelperTests.cs
+++ UnitTests/Helpers/CharacterJobEnumConverterHelperTests.cs
@@ -45,5 +45,22 @@ namespace UnitTests.Helpers
             Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
         }
 
+        [Test]
+        public void CharacterJobEnumConverterHelperTests_ConvertBack__Valid_Should_Pass()
+        {
+            // Arrange
+            var myConverter = new CharacterJobEnumConverter();
+            var myObject = "HeadChef";
+            var Expected = CharacterJobEnum.HeadChef;
+
+            // Act
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
     }
 }
