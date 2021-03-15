4cc94e28fb18620b1bcfae27d225059a0aa935a3
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:59:36 -0700

    Add ItemModelEnumHelperTests.cs to Unit Test project and add ItemModelEnumHelperTests_ConvertMessageToEnum_Should_Pass

---
+++ UnitTests/Helpers/ItemModelEnumHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+using Game.Models;
+using Game.Helpers;
+using System.Collections.Generic;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    public class ItemModelEnumHelperTests
+    {
+        [Test]
+        public void ItemModelEnumHelperTests_ConvertMessageToEnum_Should_Pass()
+        {
+            // Arrange
+            var Expected = ItemModelEnum.Unknown;
+
+            // Act
+            var Actual = ItemModelEnumHelper.ConvertMessageToEnum("Test");
+            
+
+            // Assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
