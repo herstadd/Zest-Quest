a4b4ff09d05f70c12790d8230d6cceb243f9381b
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:32:50 -0700

    Add MonsterTypeEnumHelperTests to UnitTest project

---
+++ UnitTests/Helpers/MonsterTypeEnumHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class MonsterTypeEnumHelperTests
+    {
+        
+    }
+}
