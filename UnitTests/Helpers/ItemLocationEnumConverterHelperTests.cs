a849851fe92ac1502add5c8ba4e9acbda29aa259
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:09:17 -0700

    Add ItemLocationEnumConverterHelperTests to the Unit Test project

---
+++ UnitTests/Helpers/ItemLocationEnumConverterHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Game.Models;
+using Game.Helpers;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class ItemLocationEnumConverterHelperTests
+    {
+    }
+}
