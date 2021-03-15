4ac7220d46e95c2faca3ff7e91e5c054869247ad
Ruifeng Wang <rwang@seattleu.edu>
2021-03-14 23:39:03 -0700

    Add RoundOverPage_EquipItem_Clicked_PossibleItem_RightFinger_Should_Pass to RoundOverPageTests.cs

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -792,5 +792,32 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void RoundOverPage_EquipItem_Clicked_PossibleItem_RightFinger_Should_Pass()
+        {
+            // Arrange
+            var CurrentItem = (Label)page.FindByName("CurrentItemName");
+            CurrentItem.Text = "Scream Ring";
+
+            var PossibleItem = (Label)page.FindByName("PossibleItemName");
+            PossibleItem.Text = "Scream Ring";
+
+            var PossibleItemLocation = (Label)page.FindByName("PossibleItemLocation");
+            PossibleItemLocation.Text = "Right Finger";
+
+            page.SelectedCharacter = new PlayerInfoModel
+            {
+                RightFinger = null,
+            };
+
+            // Act
+            page.EquipItem_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
