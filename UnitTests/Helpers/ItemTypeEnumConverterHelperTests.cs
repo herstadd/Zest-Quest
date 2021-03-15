480173a6d7c64a5a240b884814796d500a8f861a
JungBok Cho <chojb2015@gmail.com>
2021-03-15 00:21:26 -0700

    Add ItemTypeEnumConverterHelperTests_Convert_Valid_Should_Pass to ItemTypeEnumConverterHelperTests.cs

--- UnitTests/Helpers/ItemTypeEnumConverterHelperTests.cs
+++ UnitTests/Helpers/ItemTypeEnumConverterHelperTests.cs
@@ -10,6 +10,22 @@ namespace UnitTests.Helpers
     [TestFixture]
     class ItemTypeEnumConverterHelperTests
     {
-        
+        [Test]
+        public void ItemTypeEnumConverterHelperTests_Convert_Valid_Should_Pass()
+        {
+            // Arrange
+            var myConverter = new ItemTypeEnumConverter();
+
+            var myObject = "ButcherKnifeNecklace";
+            var Expected = "Butcher Knife Necklace";
+
+            // Act
+            var Result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
     }
 }
