6b187b4618ef2a8ac089acd40b90fbc81548500f
Ruifeng Wang <rwang@seattleu.edu>
2021-03-14 23:37:46 -0700

    Add RoundOverPage_EquipItem_Clicked_PossibleItem_OffHand_Should_Pass to RoundOverPageTests.cs

--- UnitTests/Views/Battle/RoundOverPageTests.cs
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -765,5 +765,32 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void RoundOverPage_EquipItem_Clicked_PossibleItem_OffHand_Should_Pass()
+        {
+            // Arrange
+            var CurrentItem = (Label)page.FindByName("CurrentItemName");
+            CurrentItem.Text = "Knife";
+
+            var PossibleItem = (Label)page.FindByName("PossibleItemName");
+            PossibleItem.Text = "Knife";
+
+            var PossibleItemLocation = (Label)page.FindByName("PossibleItemLocation");
+            PossibleItemLocation.Text = "Off Hand";
+
+            page.SelectedCharacter = new PlayerInfoModel
+            {
+                OffHand = null,
+            };
+
+            // Act
+            page.EquipItem_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
