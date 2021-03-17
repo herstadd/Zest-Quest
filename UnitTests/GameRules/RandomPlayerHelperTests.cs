916a508d3558bad60e4374493e508cfc60dfae1c
Sam Borhan <samborhan1566@gmail.com>
2021-03-17 14:28:02 -0700

    Remove commented lines from RandomPlayerHelperTests.cs

--- UnitTests/GameRules/RandomPlayerHelperTests.cs
+++ UnitTests/GameRules/RandomPlayerHelperTests.cs
@@ -232,103 +232,6 @@ namespace UnitTests.Helpers
             Assert.AreEqual(expected, result);
         }
 
-        //[Test]
-        //public void RandomPlayerHelper_GetRandomCharacter_InValid_Empty_CharacterList_Should_Return_New()
-        //{
-        //    // Arrange
-        //    CharacterIndexViewModel.Instance.Dataset.Clear();
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomCharacter(1);
-
-        //    // Reset
-
-        //    // Assert
-        //    Assert.AreEqual(true, result.Name.Contains("Elf"));
-        //}
-
-        //[Test]
-        //public async Task RandomPlayerHelper_GetRandomCharacter_Valid_CharacterList_1_Should_Return_1()
-        //{
-        //    // Arrange
-        //    CharacterIndexViewModel.Instance.Dataset.Clear();
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "1" });
-
-        //    // Arrange
-        //    DiceHelper.EnableForcedRolls();
-        //    DiceHelper.SetForcedRollValue(1);
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomCharacter(1);
-
-        //    // Reset
-        //    DiceHelper.DisableForcedRolls();
-
-        //    // Assert
-        //    Assert.AreEqual(true, result.UniqueItem.Equals("1"));
-        //}
-
-        //[Test]
-        //public async Task RandomPlayerHelper_GetRandomCharacter_Valid_CharacterList_3_Should_Return_2()
-        //{
-        //    // Arrange
-        //    CharacterIndexViewModel.Instance.Dataset.Clear();
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "1" });
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "2" });
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "3" });
-
-        //    DiceHelper.EnableForcedRolls();
-        //    DiceHelper.SetForcedRollValue(2);
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomCharacter(1);
-
-        //    // Reset
-        //    DiceHelper.DisableForcedRolls();
-
-        //    // Assert
-        //    Assert.AreEqual(true, result.UniqueItem.Equals("2"));
-        //}
-
-        //[Test]
-        //public async Task RandomPlayerHelper_GetRandomCharacter_Valid_Health_Should_Be_Correct()
-        //{
-        //    // Arrange
-        //    CharacterIndexViewModel.Instance.Dataset.Clear();
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "1" });
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "2" });
-        //    await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { UniqueItem = "3" });
-
-        //    DiceHelper.EnableForcedRolls();
-        //    DiceHelper.SetForcedRollValue(2);
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomCharacter(1);
-
-        //    // Reset
-        //    DiceHelper.DisableForcedRolls();
-
-        //    // Assert
-        //    Assert.AreEqual(result.MaxHealth, result.CurrentHealth);
-        //}
-
-        //[Test]
-        //public void RandomPlayerHelper_GetRandomMonster_InValid_Empty_List_Should_Return_New_Monster()
-        //{
-        //    // Arrange
-        //    var save = MonsterIndexViewModel.Instance.Dataset;
-        //    MonsterIndexViewModel.Instance.Dataset.Clear();
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomMonster(1);
-
-        //    // Reset
-        //    MonsterIndexViewModel.Instance.Dataset = save;
-
-        //    // Assert
-        //    Assert.AreEqual(PlayerTypeEnum.Monster, result.PlayerType);
-        //}
-
         [Test]
         public void RandomPlayerHelper_GetRandomMonster_Valid_Should_Return_New_Monster()
         {
@@ -355,25 +258,7 @@ namespace UnitTests.Helpers
 
             // Assert
             Assert.AreEqual(PlayerTypeEnum.Monster, result.PlayerType);
-        }
-
-        //[Test]
-        //public async Task RandomPlayerHelper_GetRandomMonster_Valid_Items_True_Should_Return_New_Monster()
-        //{
-        //    // Arrange
-        //    MonsterIndexViewModel.Instance.Dataset.Clear();
-        //    await MonsterIndexViewModel.Instance.CreateAsync(new MonsterModel { UniqueItem = "1" });
-        //    await MonsterIndexViewModel.Instance.CreateAsync(new MonsterModel { UniqueItem = "2" });
-        //    await MonsterIndexViewModel.Instance.CreateAsync(new MonsterModel { UniqueItem = "3" });
-
-        //    // Act
-        //    var result = RandomPlayerHelper.GetRandomMonster(1,true);
-
-        //    // Reset
-
-        //    // Assert
-        //    Assert.AreEqual(PlayerTypeEnum.Monster, result.PlayerType);
-        //}
+        }       
 
         [Test]
         public void RandomPlayerHelper_GetRandomMonster_Valid_Items_False_Should_Return_New_Monster()
