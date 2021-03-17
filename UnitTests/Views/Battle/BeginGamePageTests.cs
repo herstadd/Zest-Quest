9d8a1b226a7b28e1ce75c734d4c112d4eb7f4ab5
Ruifeng Wang <rwang@seattleu.edu>
2021-03-17 14:58:26 -0700

    Remove unnecessary using in BeginGamePageTests.cs

--- UnitTests/Views/Battle/BeginGamePageTests.cs
+++ UnitTests/Views/Battle/BeginGamePageTests.cs
@@ -1,17 +1,10 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms.Mocks;
 using Xamarin.Forms;
 
 using Game;
 using Game.Views;
-using Game.Models;
 using Game.ViewModels;
 
 namespace UnitTests.Views
